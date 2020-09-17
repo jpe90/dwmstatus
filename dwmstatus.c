e2e5d44273631570e9d9b041f90ea3b2a2ced61f
jpe90 <patriot.game@gmail.com>
2020-09-16 21:44:07 -0400

    change to new york time

--- dwmstatus.c
+++ dwmstatus.c
@@ -17,7 +17,7 @@
 
 #include <X11/Xlib.h>
 
-char *tzargentina = "America/Buenos_Aires";
+char *tznewyork = "America/New_York";
 char *tzutc = "UTC";
 char *tzberlin = "Europe/Berlin";
 
@@ -182,10 +182,8 @@ main(void)
 	char *avgs;
 	char *bat;
 	char *bat1;
-	char *tmar;
-	char *tmutc;
-	char *tmbln;
-	char *t0, *t1, *t2;
+    char *tmny;
+    char *t0, *t1, *t2;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
@@ -196,17 +194,14 @@ main(void)
 		avgs = loadavg();
 		bat = getbattery("/sys/class/power_supply/BAT0");
 		bat1 = getbattery("/sys/class/power_supply/BAT1");
-		tmar = mktimes("%H:%M", tzargentina);
-		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
-		t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
+        tmny = mktimes("KW %W %a %d %b %H:%M %Z %Y", tznewyork);
+        t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
 		t1 = gettemperature("/sys/devices/virtual/hwmon/hwmon2", "temp1_input");
 		t2 = gettemperature("/sys/devices/virtual/hwmon/hwmon4", "temp1_input");
 
-		status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s",
-				t0, t1, t2, avgs, bat, bat1, tmar, tmutc,
-				tmbln);
-		setstatus(status);
+                status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s", t0,
+                                  t1, t2, avgs, bat, bat1, tmny);
+                setstatus(status);
 
 		free(t0);
 		free(t1);
@@ -214,10 +209,8 @@ main(void)
 		free(avgs);
 		free(bat);
 		free(bat1);
-		free(tmar);
-		free(tmutc);
-		free(tmbln);
-		free(status);
+        free(tmny);
+        free(status);
 	}
 
 	XCloseDisplay(dpy);
